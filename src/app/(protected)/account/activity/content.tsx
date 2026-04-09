d3859b3377be0f2211ce352c85312db4cfa5b44b
musabatas <musabatas@gmail.com>
2026-04-10 02:27:48 +0300

    Purge dead code: 105 unused files, ~26K lines removed
    - Delete 91 starter-kit account pages (not linked from navigation)
    - Delete 2 starter-kit auth pages (get-started, welcome-message)
    - Delete entire partials/ directory (132 of 133 files dead)
    - Delete 11 unused shadcn/ui components (kanban, drawer, toggle, etc.)
    - Delete 3 unused hooks, 1 dead common component, image-input/
    - Sole survivor (user-dropdown-menu.tsx) moved to components/common/

--- src/app/(protected)/account/activity/content.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export function AccountActivityContent() {
-  return (
-    <div className="text-center py-12 text-muted-foreground">
-      <p>Activity log coming soon</p>
-    </div>
-  );
-}
